eae62b03906cf8a9e1dc84129e2e4e3b70808fe7
Sean Silva <silvasean@google.com>
2022-09-21 09:59:06 +0000

    Add initial support for large external weights.
    PyTorch 2.0 will make this largely obsolete, since Dynamo-based
    workflows don't bake the weights into the graph at all. This will
    present other problems for AoT flows but we still need to align with
    PyTorch's story there.
    
    What this patch implements is a way to tunnel symbolic external
    references through our current TorchScript frontend. It also lowers them
    from the backend contract to the linalg-on-tensors backend contract via
    `ml_program.global` (this also applies in principle to MHLO and TOSA,
    but we don't plumb this through just yet -- it should just be a matter
    of adding the correct pass to the relevant pass pipeline).
    
    ```
    class HasLargeGlobalTensorModule(torch.nn.Module):
        def __init__(self):
            super().__init__()
            self.big = torch.ones(512, 1024)
    
        def forward(self):
            return self.big
    
    print(torch_mlir.compile(HasLargeGlobalTensorModule(), [],
                             use_external_references_if_numel_exceeds=16))
    
    Output:
    func.func @forward() -> !torch.vtensor<[512,1024],f32> {
      %0 = torch.vtensor.external.literal(@big) : !torch.vtensor<[512,1024],f32>
      return %0 : !torch.vtensor<[512,1024],f32>
    }
    ```
    
    Example use like this:
    
    ```
    import torch
    import torch_mlir
    import torchvision
    
    resnet18 = torchvision.models.resnet18(pretrained=True)
    resnet18.eval()
    
    mlir_module = torch_mlir.compile(
        resnet18, torch.ones(1, 3, 224, 224), output_type="torch",
        use_external_references_if_numel_exceeds=1)
    print(mlir_module)
    ```
    
    Backend contract IR: https://gist.github.com/silvasean/aba4b3b01b5a5649f1216dc6dd6942d4
    Linalg-on-tensors IR with `ml_program.global`: https://gist.github.com/silvasean/5ed3b6de238995e232ef3e2cb5a9f609
    
    See the documentation for `torch.vtensor.external.literal` for more
    details on the semantics.
    
    Note that this feature is too complex to support in RefBackend, so we
    unfortunately cannot not add e2e tests for this. This feature is
    expected to be superceded by the PyTorch 2 integration work since with
    Dynamo we never see the weights, so there is a bounded (albeit somewhat
    long) time horizon for its removal.

--- lib/Conversion/Passes.cpp
+++ lib/Conversion/Passes.cpp
@@ -14,12 +14,13 @@
 #include "transforms/passes.h"
 #endif // TORCH_MLIR_ENABLE_STABLEHLO
 
+#include "torch-mlir/Conversion/TorchConversionToMLProgram/TorchConversionToMLProgram.h"
+#include "torch-mlir/Conversion/TorchToMLProgram/TorchToMLProgram.h"
 #include "torch-mlir/Conversion/TorchToLinalg/TorchToLinalg.h"
 #include "torch-mlir/Conversion/TorchToSCF/TorchToSCF.h"
 #include "torch-mlir/Conversion/TorchToArith/TorchToArith.h"
 #include "torch-mlir/Conversion/TorchToTosa/TorchToTosa.h"
 #include "torch-mlir/Conversion/TorchToTMTensor/TorchToTMTensor.h"
-#include "torch-mlir/Conversion/TorchConversionToMLProgram/TorchConversionToMLProgram.h"
 
 //===----------------------------------------------------------------------===//
 // Pass registration
